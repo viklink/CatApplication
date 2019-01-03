7b0b94c21a492330932d084ff2ab278d7c481267
Viktoriya <Viktoriya.vyazemska@gmail.com>
2019-01-03 21:55:14 +0200

    Add ViewPager to DetailActivity

--- app/src/main/java/exam/catapp/sourceit/catapplication/DetailActivity.java
+++ app/src/main/java/exam/catapp/sourceit/catapplication/DetailActivity.java
@@ -5,10 +5,17 @@ import android.arch.persistence.room.Room;
 import android.databinding.DataBindingUtil;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.List;
+
+import exam.catapp.sourceit.catapplication.adapter.CatFragmentAdapter;
 import exam.catapp.sourceit.catapplication.database.CatDatabase;
 import exam.catapp.sourceit.catapplication.databinding.ActivityDetailBinding;
 import exam.catapp.sourceit.catapplication.model.Cat;
@@ -37,11 +44,46 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-    }
+        final ViewPager viewPager = findViewById(R.id.viewPager);
 
+        catDatabase.catDao().getLiveDataAll().observe(this, new Observer<List<Cat>>() {
+            @Override
+            public void onChanged(@Nullable List<Cat> cats) {
+                CatFragmentAdapter catFragmentAdapter = new CatFragmentAdapter(getSupportFragmentManager(), cats);
+                viewPager.setAdapter(catFragmentAdapter);
+            }
+        });
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("id", id);
     }
+
+    /*private class CatFragmentAdapter extends FragmentStatePagerAdapter {
+        private List<Cat> catList;
+
+        public CatFragmentAdapter(FragmentManager fm, List<Cat> cats) {
+            super(fm);
+            catList = cats;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+
+            Cat cat = catList.get(i);
+            CatFragment catFragment = new CatFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", cat.getId());
+            catFragment.setArguments(args);
+
+            return catFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return catList != null ? catList.size() : 0;
+        }
+    }
+    */
 }
